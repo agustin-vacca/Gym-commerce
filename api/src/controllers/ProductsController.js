b9792ee44c8a0337db005cb2c5d6ae01231ae270
agustin-vacca <112440788+agustin-vacca@users.noreply.github.com>
2023-04-12 08:56:41 -0300

    add(handler/controller): Prod and Cat with get and post

--- api/src/controllers/ProductsController.js
+++ api/src/controllers/ProductsController.js
@@ -1,14 +1,14 @@
 const {Producto, Categoria} = require('../db');
 
-const createProducts =  (id, name, price, weight, description, image, category, stock, create_date) =>
-    Producto.create({id, name, price, weight, description, image, category, stock, create_date});
+const createProducts =  ( name, price, weight, description, image, category, stock, create_date) =>
+    Producto.create({ name, price, weight, description, image, category, stock, create_date});
 
 const getProducts = () => {
     const product = Producto.findAll({ 
         include: [
             {
                 model: Categoria,
-                attributes: ["id","name","description"],
+                attributes: ["name"],
                 through: { attributes: [] },
             } 
         ]
