dda76312bfb04f0f84bc3d8854cd139e7ccc7364
agustin-vacca <112440788+agustin-vacca@users.noreply.github.com>
2023-04-12 11:10:41 -0300

    feat: :sparkles: add new styled_components

--- client/src/componentes/Home/Home.jsx
+++ client/src/componentes/Home/Home.jsx
@@ -1,56 +1,51 @@
-import React,{ useEffect} from "react";
-import { useDispatch, useSelector} from "react-redux";
-import {getCharacters} from "../../redux/actions"
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCharacters } from "../../redux/actions";
 import Card from "../Card/Card";
-import style from "./Home.module.css"
-import NavBar from "../NavBar/NavBar"
-import Footer from "../Footer/Footer"
-
+import style from "./Home.module.css";
+import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
+import { Layout } from "../common/styled_components/commonStyles";
 
 const Home = () => {
-const dispatch = useDispatch();
-const allCharacters = useSelector((state) => state.characters)
+  const dispatch = useDispatch();
+  const allCharacters = useSelector((state) => state.characters);
 
-
-useEffect(() => {
-  dispatch(getCharacters())
-   }, [dispatch]);
+  useEffect(() => {
+    dispatch(getCharacters());
+  }, [dispatch]);
 
   return (
-    <div>
-<div className={style.allNavBars}>
-    <div className={style.NavBarDiv}>
-      <h1 className={style.ThisIsHome} >Home</h1>
-      <div>
-        <img src="CardinalLogo.png" alt="."/>
+    <Layout >
+      <div className={style.allNavBars}>
+        <div className={style.NavBarDiv}>
+          <h1 className={style.ThisIsHome}>Home</h1>
+          <div>
+            <img src="CardinalLogo.png" alt="." />
+          </div>
+          <NavBar />
+          <h3 className={style.ThisIsLog}>INGRESAR/REGISTRARSE</h3>
+        </div>
+        <div className={style.divTwo}>
+          <button className={style.navButton}>Inicio</button>
+          <button className={style.navButton}>Productos</button>
+          <button className={style.navButton}>Preguntas Frecuentes</button>
+          <button className={style.navButton}>Nosotros</button>
+        </div>
       </div>
-      <NavBar/>
-      <h3 className={style.ThisIsLog}>INGRESAR/REGISTRARSE</h3>
-    </div>
-    <div className={style.divTwo}>
-      <button className={style.navButton}>Inicio</button>
-      <button className={style.navButton}>Productos</button>
-      <button className={style.navButton}>Preguntas Frecuentes</button>
-      <button className={style.navButton}>Nosotros</button>
-    </div>
-</div>
 
-    <div className={style.cardDisplayer}>
-    {
-  allCharacters.map(el=>{
-    return <Card 
-    key={el.id}
-    id={el.id}
-    name={el.name}
-    email={el.email}
-    />
-  })}
-  </div>
+      <div className={style.cardDisplayer}>
+        {allCharacters.map((el) => {
+          return (
+            <Card key={el.id} id={el.id} name={el.name} email={el.email} />
+          );
+        })}
+      </div>
 
-<div className={style.FooterDiv}>
-    <Footer />
-  </div>
-  </div>
-  )
+      <div className={style.FooterDiv}>
+        <Footer />
+      </div>
+    </Layout>
+  );
 };
 export default Home;
