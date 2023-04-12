40000d2ffbc4047cb3e07e6d832213a0ab27e045
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-04-12 13:53:50 -0300

    CardsConectedToBack

--- client/src/componentes/Card/Card.jsx
+++ client/src/componentes/Card/Card.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import style from "../Card/Card.module.css";
 
-function Card({ id, name, email }) {
+function Card({ id, name, price }) {
   return (
     <div>
       <div className={style.card}>
-        <Link to={`/detail/${id}`}>
-          <p>email : {email}</p>
-        </Link>
-        <p>name : {name}</p>
         <p>id : {id}</p>
+        <p>price : {price}</p>
+        <p>name : {name}</p>
+        <Link to={`/detail/${id}`}>
+          <p>VER MAS</p>
+        </Link>
       </div>
     </div>
   );
