bcce80b9fa5060a98183260123f228f425d3baff
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-04-12 00:38:21 -0300

    NavBar and Footer

--- client/src/componentes/Home/Home.jsx
+++ client/src/componentes/Home/Home.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector} from "react-redux";
 import {getCharacters} from "../../redux/actions"
 import Card from "../Card/Card";
 import style from "./Home.module.css"
+import NavBar from "../NavBar/NavBar"
+import Footer from "../Footer/Footer"
 
 
 const Home = () => {
@@ -16,8 +18,17 @@ useEffect(() => {
 
   return (
     <div>
-      <h1>Hola este es Home</h1>
-      <div className={style.cardDisplayer}>
+      
+    <div className={style.NavBarDiv}>
+      <h1 className={style.ThisIsHome} >Home</h1>
+      <div>
+        <img src="CardinalLogo.png" alt="."/>
+      </div>
+      <NavBar/>
+      <h3 className={style.ThisIsLog}>INGRESAR/REGISTRARSE</h3>
+    </div>
+
+    <div className={style.cardDisplayer}>
     {
   allCharacters.map(el=>{
     return <Card 
@@ -27,12 +38,12 @@ useEffect(() => {
     email={el.email}
     />
   })}
-      </div>
+  </div>
 
-
-    </div>
-
-
-  );
+<div className={style.FooterDiv}>
+    <Footer />
+  </div>
+  </div>
+  )
 };
 export default Home;
