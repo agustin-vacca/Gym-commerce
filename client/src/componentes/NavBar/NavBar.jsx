c2e9f5a64ed43a46a5fa99d9945aeba0f3fe873b
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-04-14 14:53:13 -0300

    viernes

--- client/src/componentes/NavBar/NavBar.jsx
+++ client/src/componentes/NavBar/NavBar.jsx
@@ -1,74 +1,58 @@
-
-//import React, { useEffect, useState } from "react";
 import React, { useState } from "react";
-//import { Layout } from "./NavbarStyle";
-import SearchBar from "../SearchBar/SearchBar"
-import logo from "../../cardigan.png"
+import SearchBar from "../SearchBar/SearchBar";
 import { BiUserCircle } from "react-icons/bi";
 import { Link } from "react-router-dom";
-import SearchBar from "../SearchBar/SearchBar";
-import { Layout } from "./NavbarStyle";
 import imagen from "../../cardigan.png";
+import "./NavBar.css";
 
 export default function NavBar() {
+  const [open, setOpen] = useState(false);
+
+  //NAVBAR
   return (
-    <Layout>
-      <div className="SearchBarDiv">
-        <Link to={`/`}> <img src={imagen} alt="." width="70px" /> </Link>
+    <div className="LayoutNavbar">
+      <div className="divOne">
+        <Link to={`/`}>
+          <img src={imagen} alt="." width="70px" />
+        </Link>
         <SearchBar />
         <h3 className="ThisIsLog">
-          INGRESAR/REGISTRARSE <BiUserCircle size={40} />{" "}
+          INGRESAR/REGISTRARSE <BiUserCircle size={40} />
+          {""}
         </h3>
       </div>
       <div className="divTwo">
         <div className="navButton">Inicio</div>
-        <div className="navButton">Productos</div>
+        <div className="dropDownContainer">
+          <div
+            className="navButton"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            Productos
+          </div>
+          <div className={`dropdown-menu ${open ? "active" : "inactive"}`}>
+            <ul>
+              <DropDownItem text={"Maquienas"} />
+              <DropDownItem text={"Mancuernas"} />
+              <DropDownItem text={"Rack"} />
+              <DropDownItem text={"Discos y Barras"} />
+              <DropDownItem text={"Accesorios"} />
+            </ul>
+          </div>
+        </div>
         <div className="navButton">Preguntas Frecuentes</div>
         <div className="navButton">Nosotros</div>
       </div>
-    </Layout>
-  );
-    //NAVBAR
-    return (
-    <div className="Layout">
-        <div className="SearchBarDiv">
-          <Link to={`/`}>
-          <img src={logo} alt="." width="70px"/>
-          </Link>
-          <SearchBar />
-          <h3 className="ThisIsLog" >INGRESAR/REGISTRARSE <BiUserCircle size={40}/> </h3> 
-        </div>
-        <div className="divTwo">
-          <div className="navButton">Inicio</div>
-
-          <div>
-            <div className="navButton" onClick={ () => {setOpen(!open)}}>
-              <p>Productos</p>
-            </div>
-            <div className={`dropdown-menu ${open? 'active' : 'inactive'}`}>
-              <ul>
-                <DropDownItem text = {"Maquienas"} />
-                <DropDownItem text = {"Mancuernas"} />
-                <DropDownItem text = {"Rack"} />
-                <DropDownItem text = {"Discos y Barras"} />
-                <DropDownItem text = {"Accesorios"} />
-              </ul>
-            </div>
-          </div>
-          
-          
-          <div className="navButton">Preguntas Frecuentes</div>
-          <div className="navButton">Nosotros</div>
-        </div>
     </div>
-    )
-} 
-
-function DropDownItem(props){
-  return(
-    <li className="dropdownItem" > 
-      <p> {props.text} </p>
-    </li>
-  )
+  );
 }
 
+function DropDownItem(props) {
+  return (
+    <li className="dropdownItem">
+      <p> {props.text} </p>
+    </li>
+  );
+};
