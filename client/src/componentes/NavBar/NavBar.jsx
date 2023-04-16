53cbdfbcab7eea142cb8ff08240fa516bd975227
brixz <diazb6904@gmail.com>
2023-04-16 16:38:53 -0300

    arreglando las targetas

--- client/src/componentes/NavBar/NavBar.jsx
+++ client/src/componentes/NavBar/NavBar.jsx
@@ -1,29 +1,21 @@
-<<<<<<< HEAD
 import React from "react";
-//import { BiUserCircle } from "react-icons/bi";
+import { BiUserCircle } from "react-icons/bi";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { filterProductsByCategories } from "../../redux/actions";
 import logo from "../../cardigan.png";
 import SearchBar from "../SearchBar/SearchBar";
 import UserButton from "../UserButton/UserButton";
 import { Layout } from "./NavbarStyle";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
-=======
-import React, { useState } from "react";
-import SearchBar from "../SearchBar/SearchBar"
-import logo from "../../cardigan.png"
-import { BiUserCircle } from "react-icons/bi";
-import { Link } from "react-router-dom";
-import './NavBar.css';
-import { filterProductsByCategories } from "../../redux/actions.js"
-import { useDispatch } from "react-redux";
->>>>>>> 531c3b52f3cf62661d178815589e0c41ad287216
 
 export default function NavBar(){
     // DIV EMERGENTE
     const [open, setOpen] = useState(false);
 
     // FILTROS CATEGORIA
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     const handleFilterCategories = (event) => {
       dispatch(filterProductsByCategories(event.target.value))
@@ -65,21 +57,11 @@ export default function NavBar(){
               </ul>
             </div>
           </div>
-          <div className="navButton">Preguntas Frecuentes</div>
-          <div className="navButton">Nosotros</div>
+          <div className="navButton"><Link to="/consultas" className="nosotros">Preguntas frecuentes</Link></div>
+        <div className="navButton"><Link to ="/nosotros" className="nosotros">Nosotros</Link></div>
+      
         </div>
       </div>
-<<<<<<< HEAD
-
-      <div className="divTwo">
-        <div className="navButton">Inicio</div>
-        <div className="navButton">Productos</div>
-        <div className="navButton"><Link to="/consultas" className="nosotros">Preguntas frecuentes</Link></div>
-        <div className="navButton"><Link to ="/nosotros" className="nosotros">Nosotros</Link></div>
-      </div>
-    </Layout>
-=======
->>>>>>> 531c3b52f3cf62661d178815589e0c41ad287216
   );
 }
 
