dbb25564cc18868b117cee8e9e042e39a3de7acd
agustin-vacca <112440788+agustin-vacca@users.noreply.github.com>
2023-04-24 19:09:15 -0300

    feat: :sparkles: add getIdReviews and delete average
    delete prop average and add ruotes params for id

--- api/src/routes/ReviewsRoutes.js
+++ api/src/routes/ReviewsRoutes.js
@@ -3,11 +3,12 @@ const { Router } = require('express');
 const ReviewsRoutes = Router();
 
 //Importo los handlers con la logica de las rutas
-const { getReviewsHandler, postReviewHandler, deleteReviewHandler } = require('../handlers/ReviewsHandlers')
+const { getReviewsHandler, postReviewHandler, deleteReviewHandler, getReviewByIdHandler } = require('../handlers/ReviewsHandlers')
  
 ReviewsRoutes.get('/', getReviewsHandler);
 ReviewsRoutes.post('/create', postReviewHandler);
 ReviewsRoutes.delete('/:id', deleteReviewHandler);
+ReviewsRoutes.get('/:id', getReviewByIdHandler);
 
 
 module.exports = ReviewsRoutes;
