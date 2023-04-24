7000643a29dae6c26911b9b2280b381591eed151
chato-cebes <javancebes@gmail.com>
2023-04-24 14:02:35 -0300

    Componente Carrito con la vinculacion en el navBar

---
+++ client/src/componentes/Carrito/CarritoStyle.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const CarritoItems = styled.div``;
+
+export const CarritoItem = styled.div``;
+
+export const MyCarrito = styled.div``;
+
+export const CarritoItemDetalle = styled.div``;
+
+export const CarritoHeader = styled.div``;
