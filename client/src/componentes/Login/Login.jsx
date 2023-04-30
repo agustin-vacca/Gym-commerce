0834501e9d5888aaa03690ee47b8d0fdce677179
agustin-vacca <112440788+agustin-vacca@users.noreply.github.com>
2023-04-29 22:31:27 -0300

    intento de implementacion para crear usuarios

--- client/src/componentes/Login/Login.jsx
+++ client/src/componentes/Login/Login.jsx
@@ -1,9 +1,16 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import React  from "react";
+//import { createUsers } from "../../redux/actions";
+//import { useDispatch } from "react-redux";
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
+  //const dispatch = useDispatch();
 
+  // const addUserHandler = () => {
+  //   dispatch(createUsers(user))
+  // }
+  
   return <div onClick={() => loginWithRedirect()}> Login </div>;
 };
 export default Login;
