b60f5e279c3791968d6467feb6b9b827a77adcf5
agustin-vacca <112440788+agustin-vacca@users.noreply.github.com>
2023-05-02 15:40:31 -0300

    eliminando comentarios extras

--- client/src/componentes/Login/Login.jsx
+++ client/src/componentes/Login/Login.jsx
@@ -1,15 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React  from "react";
-//import { createUsers } from "../../redux/actions";
-//import { useDispatch } from "react-redux";
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
-  //const dispatch = useDispatch();
-
-  // const addUserHandler = () => {
-  //   dispatch(createUsers(user))
-  // }
+  
   
   return <div onClick={() => loginWithRedirect()}> Login </div>;
 };
