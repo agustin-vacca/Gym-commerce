2de928d531d2c9ef0aa5b2b6aa9fb2a9d6e67dc3
agustin-vacca <112440788+agustin-vacca@users.noreply.github.com>
2023-05-03 17:58:08 -0300

    rutas admin protegidas

--- client/src/componentes/RutasProtegidas/RutasAdmin.jsx
+++ client/src/componentes/RutasProtegidas/RutasAdmin.jsx
@@ -1,21 +1,26 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { filterByAdmin } from "../../redux/actions";
 
 function RutasAdmin() {
   const { user, isAuthenticated, loginWithRedirect } = useAuth0();
   console.log("user usuario", user);
+  const dispatch = useDispatch();
+  const admins = useSelector( e => e.admins)
+  const correos = []
+  let arrayCorreos = admins.map( e => correos.push(e.email) );
 
-  const arrayCorreos = [
-    "cardigangymproducts@gmail.com",
-    "inmobaterealestate@gmail.com",
-  ];
+  useEffect( () => {
+    dispatch(filterByAdmin())
+  },[user])
 
   if (isAuthenticated) {
-    if (!arrayCorreos.includes(user.email)) {
+    if (!correos.includes(user.email)) {
       return (
         alert("No tiene permisos para ingresar a esta seccion"),
-        (<Navigate to="/" />)
+        (<Navigate to="/home" />)
       );
     }
     return <Outlet />;
